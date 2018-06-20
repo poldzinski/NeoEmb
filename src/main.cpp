d0eff4f040ccfb011564e1c0c23247ec297ae150
poldzinski <polda@poczta.onet.pl>
2018-06-20 12:18:35 -0700

    Sources moved to /src folder.

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+/////////////////////////////////////
+//
+// Main project file.
+//
+// 20-Jun-2018 Initial version.
+//
+/////////////////////////////////////
+
+// System includes
+#include <iostream>
+
+// C++ includes
+// (none)
+
+// C includes
+// (none)
+
+// Forward references
+// (none)
+
+namespace NeoEmb
+{
+
+/// <summary>Main entry point.</summary>
+/// <param name="argumentsCount">Number of input arguments.</param>
+/// <param name="ppArguments">Input arguments.</param>
+/// <returns>Exit result of the application.</returns>
+int main( int argumentsCount, char** ppArguments )
+{
+    std::cout << "Test";
+    
+    return 0;
+}
+
+} // namespace NoeEmb
