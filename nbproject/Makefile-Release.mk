7c4176d2e895c122a5b6bc097f8544a42f3116b8
poldzinski <polda@poczta.onet.pl>
2018-07-01 15:08:19 -0700

    Unit tests added.

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -36,8 +36,19 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 # Object Files
 OBJECTFILES= \
 	${OBJECTDIR}/src/Network/BaseNetworkServer.o \
+	${OBJECTDIR}/src/Network/TcpNetworkServer.o \
 	${OBJECTDIR}/src/main.o
 
+# Test Directory
+TESTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tests
+
+# Test Files
+TESTFILES= \
+	${TESTDIR}/TestFiles/f1
+
+# Test Object Files
+TESTOBJECTFILES= \
+	${TESTDIR}/src_test/Network/TcpNetworkServerTests.o
 
 # C Compiler Flags
 CFLAGS=
@@ -68,6 +79,11 @@ ${OBJECTDIR}/src/Network/BaseNetworkServer.o: src/Network/BaseNetworkServer.cpp
 	${RM} "$@.d"
 	$(COMPILE.cc) -O2 -Werror -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Network/BaseNetworkServer.o src/Network/BaseNetworkServer.cpp
 
+${OBJECTDIR}/src/Network/TcpNetworkServer.o: src/Network/TcpNetworkServer.cpp
+	${MKDIR} -p ${OBJECTDIR}/src/Network
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -Werror -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Network/TcpNetworkServer.o src/Network/TcpNetworkServer.cpp
+
 ${OBJECTDIR}/src/main.o: src/main.cpp
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} "$@.d"
@@ -75,6 +91,69 @@ ${OBJECTDIR}/src/main.o: src/main.cpp
 
 # Subprojects
 .build-subprojects:
+
+# Build Test Targets
+.build-tests-conf: .build-tests-subprojects .build-conf ${TESTFILES}
+.build-tests-subprojects:
+
+${TESTDIR}/TestFiles/f1: ${TESTDIR}/src_test/Network/TcpNetworkServerTests.o ${OBJECTFILES:%.o=%_nomain.o}
+	${MKDIR} -p ${TESTDIR}/TestFiles
+	${LINK.cc} -o ${TESTDIR}/TestFiles/f1 $^ ${LDLIBSOPTIONS}   
+
+
+${TESTDIR}/src_test/Network/TcpNetworkServerTests.o: src_test/Network/TcpNetworkServerTests.cpp 
+	${MKDIR} -p ${TESTDIR}/src_test/Network
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -Werror -I. -std=c++11 -MMD -MP -MF "$@.d" -o ${TESTDIR}/src_test/Network/TcpNetworkServerTests.o src_test/Network/TcpNetworkServerTests.cpp
+
+
+${OBJECTDIR}/src/Network/BaseNetworkServer_nomain.o: ${OBJECTDIR}/src/Network/BaseNetworkServer.o src/Network/BaseNetworkServer.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/Network
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/src/Network/BaseNetworkServer.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.cc) -O2 -Werror -std=c++11 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Network/BaseNetworkServer_nomain.o src/Network/BaseNetworkServer.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/src/Network/BaseNetworkServer.o ${OBJECTDIR}/src/Network/BaseNetworkServer_nomain.o;\
+	fi
+
+${OBJECTDIR}/src/Network/TcpNetworkServer_nomain.o: ${OBJECTDIR}/src/Network/TcpNetworkServer.o src/Network/TcpNetworkServer.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/Network
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/src/Network/TcpNetworkServer.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.cc) -O2 -Werror -std=c++11 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Network/TcpNetworkServer_nomain.o src/Network/TcpNetworkServer.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/src/Network/TcpNetworkServer.o ${OBJECTDIR}/src/Network/TcpNetworkServer_nomain.o;\
+	fi
+
+${OBJECTDIR}/src/main_nomain.o: ${OBJECTDIR}/src/main.o src/main.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/src/main.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.cc) -O2 -Werror -std=c++11 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/main_nomain.o src/main.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/src/main.o ${OBJECTDIR}/src/main_nomain.o;\
+	fi
+
+# Run Test Targets
+.test-conf:
+	@if [ "${TEST}" = "" ]; \
+	then  \
+	    ${TESTDIR}/TestFiles/f1 || true; \
+	else  \
+	    ./${TEST} || true; \
+	fi
 
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
