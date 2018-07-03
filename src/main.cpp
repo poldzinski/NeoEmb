fd6fe17d16dbc42fd11638b19c9f368079cdaaaf
poldzinski <polda@poczta.onet.pl>
2018-07-03 13:18:44 -0700

    Minimal structure for the main threads implemented.

--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,49 @@
 
 // System includes
 #include <iostream>
+#include <thread>
 
 // C++ includes
-// (none)
+#include "Core/GeneralState.hpp"
 
 // C includes
-// (none)
+#include <assert.h>
 
 // Forward references
-// (none)
+namespace Core
+{
+void NetworkThread( GeneralState* pGeneralState );
+}
 
 /// <summary>Main entry point.</summary>
 /// <param name="argumentsCount">Number of input arguments.</param>
 /// <param name="ppArguments">Input arguments.</param>
 /// <returns>Exit result of the application.</returns>
-int main( int argumentsCount, char** ppArguments )
+int main( int, char** )
 {
-    std::cout << "Test";
+    std::cout << "Starting..." << std::endl;
+
+    Core::GeneralState* pGeneralState = new Core::GeneralState();
+    assert( pGeneralState != nullptr );
+
+    std::cout << "Creating threads..." << std::endl;
+
+    std::thread networkThread = std::thread( Core::NetworkThread, pGeneralState );
+    
+    std::cout << "Core processing..." << std::endl;
+    
+    while ( pGeneralState->IsGlobalShutdown() == false )
+    {
+        pGeneralState->SetGlobalShutdown( true );
+    }
+    
+    std::cout << "Closing threads..." << std::endl;
+    
+    networkThread.join();
+
+    std::cout << "Shutting down" << std::endl;
+    
+    delete pGeneralState;
     
     return 0;
 }
