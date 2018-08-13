6dfd1faf2354e5a1f0dbd48b560060476119f10b
poldzinski <polda@poczta.onet.pl>
2018-08-13 12:39:03 -0700

    Network thread addded.

--- src/Core/NetworkThread.cpp
+++ src/Core/NetworkThread.cpp
@@ -11,7 +11,11 @@
 
 // C++ includes
 #include "Core/GeneralState.hpp"
+#include "Core/NetworkThread.hpp"
 #include "Network/TcpNetworkServer.hpp"
+#include "Network/AcceptClientCallback.hpp"
+#include "Network/ClientReadCallback.hpp"
+#include "Network/DisconnectClientCallback.hpp"
 
 // C includes
 #include <assert.h>
@@ -21,53 +25,70 @@
 
 namespace Core
 {
-    
-static bool shutDown = false;
 
-bool AcceptClientCallback( const Network::BaseNetworkServer* const pServer,
-                           const Network::BaseNetworkServer::IPEndPoint client )
+Network::AcceptClientCallback NetworkThread::m_AcceptClientCallback = Network::AcceptClientCallback();
+Network::ClientReadCallback NetworkThread::m_ClientReadCallback = Network::ClientReadCallback();
+Network::DisconnectClientCallback NetworkThread::m_DisconnectClientCallback = Network::DisconnectClientCallback();
+    
+NetworkThread::NetworkThread()
 {
-    return true;
+    m_AcceptClientCallback.Set( &AcceptClient );
+    m_ClientReadCallback.Set( &ClientRead );
+    m_DisconnectClientCallback.Set( &DisconnectClient );
 }
 
-void ClientReadCallback( const Network::BaseNetworkServer* const pServer,
-                         const Network::BaseNetworkServer::IPEndPoint client,
-                         const uint8_t* const pBuffer,
-                         const uint32_t bufferSize )
+void NetworkThread::ProcessingThread( GeneralState* pGeneralState )
 {
-    const char* pQuitCommand = "QUIT";
-    
-    if ( ( bufferSize == strlen( pQuitCommand ) ) &&
-         ( memcmp( pBuffer, pQuitCommand, bufferSize ) == 0 ) )
-    {
-        shutDown = true;
-    }
-}
-
-void DisconnectClientCallback( const Network::BaseNetworkServer* const pServer,
-                               const Network::BaseNetworkServer::IPEndPoint client )
-{
-}
-    
-/// <summary>Thread for network processing.</summary>
-/// <param name="globalShutdown">Global signal for shutting down.</param>
-void NetworkThread( GeneralState* pGeneralState )
-{
-    Network::TcpNetworkServer* pServer = new Network::TcpNetworkServer();
+    Network::TcpNetworkServer* const pServer = new Network::TcpNetworkServer();
     assert( pServer != nullptr );
     
-    pServer->SetCallback( AcceptClientCallback );
-    pServer->SetCallback( ClientReadCallback );
-    pServer->SetCallback( DisconnectClientCallback );
+    pServer->SetCallback( &m_AcceptClientCallback );
+    pServer->SetCallback( &m_ClientReadCallback );
+    pServer->SetCallback( &m_DisconnectClientCallback );
     pServer->Bind( GeneralState::TCP_SERVER_PORT );
     while ( ( pGeneralState->IsGlobalShutdown() == false ) &&
-            ( shutDown == false ) )
+            ( m_ShutDown == false ) )
     {
         
     }
     pServer->Disconnect();
     
     delete pServer;
+}
+
+void NetworkThread::AcceptClient()
+{
+    GetInstance().AcceptThisClient();
+}
+
+void NetworkThread::AcceptThisClient()
+{
+    m_AcceptClientCallback.Accept();
+}
+
+void NetworkThread::ClientRead()
+{
+    GetInstance().ThisClientRead();
+}
+
+void NetworkThread::ThisClientRead()
+{
+    const char* pQuitCommand = "QUIT";
+    
+    if ( ( m_ClientReadCallback.GetBufferSize() == strlen( pQuitCommand ) ) &&
+         ( memcmp( m_ClientReadCallback.GetBuffer(), pQuitCommand, m_ClientReadCallback.GetBufferSize() ) == 0 ) )
+    {
+        m_ShutDown = true;
+    }
+}
+
+void NetworkThread::DisconnectClient()
+{
+    GetInstance().DisconnectThisClient();
+}
+
+void NetworkThread::DisconnectThisClient()
+{
 }
 
 }  // namespace Core
